ff3be7353a1251cf2c93d734634f575e3166490c
Kamil Hasiak <hasiakkam@gmail.com>
2021-10-18 01:10:58 +0200

    Add new files

---
+++ galeria.php
@@ -0,0 +1,31 @@
+<?php  
+ $connect = mysqli_connect("localhost", "root", "", "testing");  
+ if(isset($_POST["insert"]))  
+ {  
+      $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+      $query = "INSERT INTO tbl_images(name) VALUES ('$file')";  
+      if(mysqli_query($connect, $query))  
+      {  
+           echo '<script>alert("Image Inserted into Database")</script>';  
+      }  
+ }  
+ ?>  
+<table class="table table-bordered">  
+                     <tr>  
+                          <th>Image</th>  
+                     </tr>  
+                <?php  
+                $query = "SELECT * FROM tbl_images ORDER BY id DESC";  
+                $result = mysqli_query($connect, $query);  
+                while($row = mysqli_fetch_array($result))  
+                {  
+                     echo '  
+                          <tr>  
+                               <td>  
+                                    <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="30%" width="30%" class="img-thumnail" />  
+                               </td>  
+                          </tr>  
+                     ';  
+                }  
+                ?>  
+                </table>  
